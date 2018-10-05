9a7317ab880c4e8c0d46c8dfb6d615c6b825c5b9
Yu Li <yli0607x@gmail.com>
2018-10-05 17:15:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,11 +59,11 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_supplies.each do |season, holidays|
-      puts "#{season.capitalize}:"
-      holidays.each do |holiday, supplies|
-        puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
-      end
+    puts "#{season.capitalize}:"
+    holidays.each do |holiday, supplies|
+      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
     end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
